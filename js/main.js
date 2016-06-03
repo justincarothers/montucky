f173e643ce016a57ebd887729a95209d9854305f
justincarothers <justincarothers@gmail.com>
2016-06-03 16:23:31 -0700

    added logic and responsiveness

--- js/main.js
+++ js/main.js
@@ -6,9 +6,41 @@ $(document).ready(function() {
     var result1 = $('input[name="query1"]:checked').val();
     var result2 = $('input[name="query2"]:checked').val();
     var result3 = $('input[name="query3"]:checked').val();
-    alert (result1 + result2 + result3);
+
+    if (result1 === NaN || result2 === NaN || result3 === NaN ){
+      alert("CHOOSE YOUR FAVORITE THINGS!");
+    } else if (result1 === '1' || result2 === '5')  {
+      $("#optionTwo").hide();
+      $("#optionOne").hide();
+      $("#winnerOne").show();
+      $("#questionBanner").hide();
+      alert("The outdoorsy drunk. You go to jail.")
+    } else if (result3 === '9' && result2 ==='5') {
+      $("#optionTwo").hide();
+      $("#optionOne").hide();
+      $("#winnerTwo").show();
+      $("#questionBanner").hide();
+      alert("Ohhh very nice");
+    } else if (result1 === 2 || result2 === 6 || result3 === 12) {
+      $("#optionTwo").hide();
+      $("#optionOne").hide();
+      $("#questionBanner").hide();
+      $("#winnerThree").show();
+      alert("You get to visit the post office!")
+    } else {
+      $("#optionTwo").hide();
+      $("#optionOne").hide();
+      $("#winnerFour").show();
+      $("#questionBanner").hide();
+      alert("Your perfect destination is a trip to the abandoned nuclear power plant located in the heart of the Montucky radiation zone!")
+    }
   });
 
+  $("#winnerOne").hide();
+  $("#winnerTwo").hide();
+  $("#winnerThree").hide();
+  $("#winnerFour").hide();
+  $("#errorOne").hide();
   $("#optionTwo").hide();
   $("#optionThree").hide();
   $("#errorOne").hide();
@@ -66,7 +98,6 @@ $(document).ready(function() {
   })
 
   $("#hotel button").click(function(){
-    var price
     $("#optionOne").hide();
     $("#errorOne").show();
   })
