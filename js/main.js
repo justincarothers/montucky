a8ec22afeac97483f3365f1a3f5a44398aed9bb7
justincarothers <justincarothers@gmail.com>
2016-06-03 13:22:57 -0700

    added some logic to survey. this will help montucky find the perfect destination for its customers.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,41 @@
 $(document).ready(function() {
-
+  // ======================ANIMATIONS=================
   var price
   $("#optionTwo").hide();
   $("#optionThree").hide();
   $("#errorOne").hide();
 
+  $("#introBanner").animate({
+    marginLeft: "+=4000",
+  }, 0)
+  $("#introBanner").animate({
+    marginLeft: "-=4000" ,
+  }, 300);
+
+  $("img").animate({
+    marginLeft: "+=4000" ,
+  }, 0)
+  $("img").animate({
+    marginLeft: "-=4000" ,
+  }, 900);
+
+  // =============LOGIC=================
+
+  $('#myForm input').on('change', function() {
+    alert($('input[name=radioName]:checked', '#myForm').val());
+  });
 
 
+
+
+
+  // ==========================================
 
   $("#camping button").click(function(){
     var price
+    $("#optionOne").animate({
+      marginLeft: "-=4000" ,
+    }, 800);
     $("#optionOne").hide();
     $("#optionTwo").show();
   })
